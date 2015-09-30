eb30030e965b4038a134df65befdc329d2459d5c
Rafal Wadolowski <xelexin@gmail.com>
2015-09-30 14:28:05 +0200

    enable installation,configuration

--- manifests/director.pp
+++ manifests/director.pp
@@ -3,6 +3,9 @@
 class bacula::director (
   $director_package = $bacula::params::director_package
 
+  include bacula::director::install
+  include bacula::director::config
+  include bacula::director::service
 ) inherits bacula::params {
 
 
