4eb127567b1b72efee75a21f4fdc5c9cd5b34da3
Rafal Wadolowski <xelexin@gmail.com>
2015-09-30 15:18:21 +0200

    test

--- manifests/director/config.pp
+++ manifests/director/config.pp
@@ -1,9 +1,9 @@
 # == Class: class_name
 #
 class bacula::director::config inherits bacula::director {
-  file { '${director_conf}':
+  file { $director_conf:
     ensure  => file,
-    path => '${director_conf}',
+    path => $director_conf,
     mode    => '0644',
     require => Class['bacula::director::install'],
     source  => 'puppet:///modules/bacula/conf/bacula-dir.conf',
