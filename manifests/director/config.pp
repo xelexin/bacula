a031b798e34c8829a3742b598476116dee5f29f7
xelexin <rafal@rafal.net.pl>
2015-10-12 15:59:18 +0200

    test

--- manifests/director/config.pp
+++ manifests/director/config.pp
@@ -7,16 +7,24 @@ class bacula::director::config inherits bacula::director {
     mode => '0644',
   }
 
+
   concat::fragment { 'director_conf_main':
     target  => $director_conf,
     order   => '01',
     mode    => '0644',
     content => template('bacula/director/bacula-dir.conf.erb')
   }
+
+
+  concat { "puppet:///modules/bacula/files/clients"
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
 }
 define bacula::director::config::client_register($name){
   concat::fragment{ "dir_conf_fragment_$name" :
-    target => '/etc/bacula/bacula-dir.conf',
+    target => "puppet:///modules/bacula/files/clients",
     order => '10',
     content => template('bacula/client/client.erb'),
   }
