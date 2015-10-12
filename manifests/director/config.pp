2b5a41164c2d36812a73d1b13a307cc215e624fc
xelexin <rafal@rafal.net.pl>
2015-10-12 15:33:38 +0200

    fixes

--- manifests/director/config.pp
+++ manifests/director/config.pp
@@ -5,21 +5,19 @@ class bacula::director::config inherits bacula::director {
     owner => 'root',
     group => 'root',
     mode => '0644',
-    backup_fragments => true,
-    ensure => 'present',
   }
 
-  concat::fragment { 'director_conf':
+  concat::fragment { 'director_conf_main':
     target  => $director_conf,
     order   => '01',
     mode    => '0644',
-    content => template('bacula/director/bacula-dir.conf.erb')
+    source => template('bacula/director/bacula-dir.conf.erb')
   }
 }
 define bacula::director::config::client_register($name){
   concat::fragment{ "dir_conf_fragment_$name" :
     target => '/etc/bacula/bacula-dir.conf',
     order => '10',
-    content => template('bacula/client/client.erb'),
+    source => template('bacula/client/client.erb'),
   }
 }
