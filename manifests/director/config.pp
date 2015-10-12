715f1c2c9115a98c5f98ef44217a768cacb96ef7
xelexin <rafal@rafal.net.pl>
2015-10-12 11:11:47 +0200

    bug fixes

--- manifests/director/config.pp
+++ manifests/director/config.pp
@@ -11,6 +11,6 @@ class bacula::director::config inherits bacula::director {
     target  => $director_conf,
     order   => '01',
     mode    => '0644',
-    content => template('bacula/client/bacula-dir.conf.erb')
+    content => template('bacula/director/bacula-dir.conf.erb')
   }
 }
