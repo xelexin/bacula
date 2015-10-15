25701be3ef378a3d91325ec678d8f4db9852a91b
xelexin <rafal@rafal.net.pl>
2015-10-15 12:44:15 +0200

    tes

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -14,7 +14,7 @@ class bacula::client::config inherits bacula::client {
     content => template('bacula/client/bacula-fd.conf.erb')
   }
 
-  concat { "$client_conf.tmp":
+  concat { "/etc/bacula/test":
     owner => 'root',
     group => 'root',
     mode => '0644',
