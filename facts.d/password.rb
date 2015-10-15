9af0fe49ebe20e752abf3b8aba7b2de8a554d4b3
xelexin <rafal@rafal.net.pl>
2015-10-15 10:51:32 +0200

    fix

--- facts.d/password.rb
+++ facts.d/password.rb
@@ -1,6 +1,6 @@
 # password.rb
 
-Facter.add('fd_password') do
+Facter.add(':fd_password') do
   confin :kernel => 'Linux'
   setcode do
     Facter::Core::Execution.exec('tr -dc A-Za-z0-9_ < /dev/urandom | head -c32')
