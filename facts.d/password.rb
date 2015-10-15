02e890300fdc9782d4bbf60760e00a0c9350df0a
xelexin <rafal@rafal.net.pl>
2015-10-15 11:05:17 +0200

    test

--- facts.d/password.rb
+++ facts.d/password.rb
@@ -2,7 +2,6 @@
 
 Facter.add(':fd_password') do
   confine :kernel => 'Linux'
-  setcode do
-    Facter::Core::Execution.exec('tr -dc A-Za-z0-9_ < /dev/urandom | head -c32')
+  setcode 'tr -dc A-Za-z0-9_ < /dev/urandom | head -c32'
   end
 end
