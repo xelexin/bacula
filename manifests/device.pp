b747582719db5980a0d096e2ad935ad1fefa1bbd
xelexin <rafal@rafal.net.pl>
2015-10-16 12:51:47 +0200

    bug fix

--- manifests/device.pp
+++ manifests/device.pp
@@ -1,7 +1,7 @@
 # == Class: bacula::device
 #
 class bacula::device {
-  if defined(Class["bacula::storage"])
+  if !defined(Class["bacula::storage"])
     { fail('Device can by only run on storage host') }
 
 
