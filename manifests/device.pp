82ddd16d13121487e56de7cd8a21a09976952699
xelexin <rafal@rafal.net.pl>
2015-10-16 15:35:09 +0200

    test

--- manifests/device.pp
+++ manifests/device.pp
@@ -1,26 +1,2 @@
 # == Class: bacula::device
 #
-
-define bacula::device (
-  $device_name,
-  $device_path,
-
-  $device_random_access = $bacula::params::device_random_access,
-  $device_removable = $bacula::params::device_removable,
-  $device_mount = $bacula::params::device_mount,
-
-  ) {
-
-  if !defined(Class["bacula::storage"])
-    { fail('Device can by only run on storage host') }
-# inherits bacula::pa#rams
-
-
-  @@concat::fragment { "storage_device_$device_name":
-    target => $storage_conf,
-    order => '10',
-    content => template('bacula/storage/device.erb'),
-    tag => $storage_name,
-  }
-
-}
