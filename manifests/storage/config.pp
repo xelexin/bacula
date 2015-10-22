dbb5fb0c690ee1d9b69f74171002483330fc412f
xelexin <rafal@rafal.net.pl>
2015-10-22 08:52:55 +0200

    test

--- manifests/storage/config.pp
+++ manifests/storage/config.pp
@@ -14,13 +14,13 @@ class bacula::storage::config inherits bacula::storage {
     content => template('bacula/storage/bacula-sd.conf.erb')
   }
 
+  Concat::Fragment <<| tag == "device_$storage_name" |>>
+
   @@concat::fragment { "dir_conf_fragment_$storage_name":
   	target => $director_conf,
   	order => '10',
   	content => template('bacula/storage/storage.erb'),
     tag => 'storage_part',
   }
-
-  Concat::Fragment <<| tag == "device_$storage_name" |>>
 
 }
