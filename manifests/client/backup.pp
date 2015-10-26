ca2a3ffa2602983ffeb0cf95e4ea7a413f11c971
xelexin <rafal@rafal.net.pl>
2015-10-26 17:23:16 +0100

    typo

--- manifests/client/backup.pp
+++ manifests/client/backup.pp
@@ -6,7 +6,7 @@ define bacula::client::backup (
   $backup_pool,
   $backup_level = $bacula::params::backup_level,
   ) {
-#    @@concat::fragment{"$::hostname-$backup_level-$backup_files-fs":
+    @@concat::fragment{"$::hostname-$backup_level-$backup_files-fs":
       target  => $bacula::params::director_conf,
       order   => '10',
       content => template('bacula/client/fileset.erb'),
