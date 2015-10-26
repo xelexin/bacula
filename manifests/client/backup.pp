35fe4f276054bae8ebfd62b074d2c294f0a4b725
xelexin <rafal@rafal.net.pl>
2015-10-26 17:19:48 +0100

    testing new feature

--- manifests/client/backup.pp
+++ manifests/client/backup.pp
@@ -6,11 +6,14 @@ define bacula::client::backup (
   $backup_pool,
   $backup_level = $bacula::params::backup_level,
   ) {
-    @@concat::fragment{"$::hostname-$backup_level-$backup_files-fs":
+#    @@concat::fragment{"$::hostname-$backup_level-$backup_files-fs":
+    @@concat::fragment{"$::hostname-fs":
       target  => $director_conf,
       order   => '10',
       content => template('bacula/client/fileset.erb'),
       tag     => 'filesets',
     }
+
+
 
 }
